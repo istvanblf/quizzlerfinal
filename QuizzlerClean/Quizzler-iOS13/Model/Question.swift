7d0f8493a9a2aa349a7c25f17553df3ec75c860a
xPty <69746218+xPty@users.noreply.github.com>
2020-08-15 23:44:48 -0700

    Add files via upload

---
+++ QuizzlerClean/Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,18 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Isti on 8/13/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+struct Question {
+    let text: String
+    let answer : String
+    
+    init(q: String, a: String) {
+        text = q
+        answer = a
+    }
+}
